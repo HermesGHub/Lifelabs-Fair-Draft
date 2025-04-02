83ff0e37c4d13eafe1e93282178b5b760133fe28
HermesGHub <andregoleta@gmail.com>
2025-04-02 08:39:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+username = ""
+owner_username = Hermes
+editor_username = Janosa or Venice or Dominic or JR
+guest_username = Dawson
+
+input("Please enter username ")
+if username != owner_username or editor_username or guest_username: 
+  print("This username does not exist, please sign in")
+
+
